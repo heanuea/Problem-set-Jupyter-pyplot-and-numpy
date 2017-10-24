cafa7722cc1c60a04aa2ca5ee16e69920619c302
heanuea <G00318763@gmit.ie>
2017-10-24 16:40:13 +0100

    imported datsa and created simple plot

---
+++ 03..SimplePlot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+pl.rcParams['figure.figsize'] = (16.0, 8.0)
+
+
+# Load data
+OriginalData = np.loadtxt("data.csv",str, delimiter=",", skiprows=1, unpack=True)
+# Transposing the data
+data = OriginalData.transpose()
+# get each column
+sepal_length = sepal_width = petal_length = petal_width = species =[]
+sepal_length , sepal_width , petal_length , petal_width , species = OriginalData
+
+
+# Create the plot.
+pl.scatter(sepal_length, sepal_width, c='pink')
+
+# Set properties.
+pl.xlabel('sepal length')
+pl.ylabel('sepal width')
+
+# Display plots .
+pl.title('TITLE')
+pl.show()
